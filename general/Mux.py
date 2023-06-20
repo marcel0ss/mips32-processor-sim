b2f230df0c9a03ce6f26a64eb39fa5ccba873a6f
Marcelo Sanchez <marcelo.snsolano@gmail.com>
2023-06-20 14:56:52 -0600

    Add initial Mux model

---
+++ general/Mux.py
@@ -0,0 +1,23 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+class Mux:
+
+    def __init__(self, num_inputs):
+        # Create an array that holds the value of the inputs
+        self.inputs = [0x0 for _ in range(num_inputs)]
+        self.select = 0x0
+        self.output = 0x0
+        
+
+    def select(self, input):
+        
+        if input >= len(self.inputs):
+            log.error(f"Select signal is invalid ({input}). " +
+                      f"Valid options are from 0 to {len(self.inputs) - 1}. " +
+                      "Output will remain unchanged")
+            return
+            
+        self.output = self.inputs[input]
+            
