d73f090b5d9cbb6f566d83f2bd7f4beade72977c
Marcelo Sanchez <marcelo.snsolano@gmail.com>
2023-06-20 11:52:57 -0600

    Modify CI to stop linting from preventing full execution

--- config/Configurator.py
+++ config/Configurator.py
@@ -73,7 +73,7 @@ class Configurator:
         # Verify that the architecture is between the accepted range
         if arch > MAX_ARCH_SIZE or arch < MIN_ARCH_SIZE:
             log.warning(
-                f"Architecture specified is out of bounds " +
+                "Architecture specified is out of bounds " +
                 f"({MIN_ARCH_SIZE} - {MAX_ARCH_SIZE}). " +
                 f"Using the default value of {DEFAULT_ARCH_SIZE}")
             return
@@ -82,7 +82,7 @@ class Configurator:
         is_not_pwr_2 = arch & (arch - 1)
         if is_not_pwr_2:
             log.error(
-                f"Architecture must be a power of 2. " +
+                "Architecture must be a power of 2. " +
                 f"Using the default archictecture of {DEFAULT_ARCH_SIZE}")
             return
 
