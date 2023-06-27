f66297c78b41eef9cb0771bdfd232f0c942cbede
Marcelo Sanchez <marcelo.snsolano@gmail.com>
2023-06-27 08:15:48 -0600

    Add execute stage logic

--- execute/Execute.py
+++ execute/Execute.py
@@ -1,6 +1,7 @@
 import logging
 from .ALU import ALU
 from general.Mux import Mux
+from .Exmem_reg import EXMEM
 
 log = logging.getLogger(__name__)
 
@@ -12,4 +13,36 @@ class Execute:
         self.wr_reg_mux = Mux(2)
 
     def run_execution(self, idex):
-        
+        # Set muxes
+        self.alu_in_mux.set_input(0, idex.rd2_data)
+        self.alu_in_mux.set_input(1, idex.sigext_imm)
+        self.alu_in_mux.select(idex.alu_in2_mux_sel)
+
+        self.wr_reg_mux.set_input(0, idex.wr_reg)
+        self.wr_reg_mux.set_input(1, idex.shift_amt)
+        self.wr_reg_mux.select(idex.dest_reg_mux_sel)
+
+        # Set up ALU
+        self.alu.alu_op = idex.alu_op
+        self.alu.in1 = idex.rd1_data
+        self.alu.in2 = self.alu_in_mux.output
+
+        # Set jump address
+        jmp_addr = idex.next_instr_addr + \
+                    (idex.sigext_imm << 2)
+
+        # Execute
+        self.alu.execute_operation()
+
+        # Set up signals for next stage
+        exmem = EXMEM()
+        exmem.shifted_addr = jmp_addr
+        exmem.alu_zero = self.alu.zero
+        exmem.alu_output = self.alu.alu_output
+        exmem.rd2_data = idex.rd2_data
+        exmem.wr_reg_addr = self.wr_reg_mux.output
+        exmem.wr_reg_en = idex.wr_reg_en
+        exmem.jump_en = idex.jump_en
+
+
+
