b8a8078793c081e2f3fefdc246c9b7a6148d8d20
Marcelo Sanchez <marcelo.snsolano@gmail.com>
2023-06-28 10:45:22 -0600

    Remove irrelevant tests from the decode stage

--- tests/TestDecode.py
+++ tests/TestDecode.py
@@ -14,24 +14,3 @@ class TestDecode(unittest.TestCase):
     def test_correct_decode_cycle(self):
         instr = 0xAF128CD1
         idex = uut.run_decode(IFID(instr, 0x0))
-
-
-    def test_valid_reg_write(self):
-        instr = 0xAF128CD1
-        wr_data = 0xFA179830
-        uut.run_decode(IFID(instr, 0x0))
-
-        uut.write_register(uut.wr_reg, wr_data, True)
-        
-        # Verify the data was written in the register
-        self.assertEqual(uut.reg_bank.registers[uut.wr_reg].data, wr_data)
-
-    def test_reg_write_not_enabled(self):
-        instr = 0xAF128CD1
-        wr_data = 0xFA179830
-        uut.run_decode(IFID(instr, 0x0))
-
-        uut.write_register(uut.wr_reg, wr_data, False)
-        
-        # Verify the data was written in the register
-        self.assertEqual(uut.reg_bank.registers[uut.wr_reg].data, 0x0)
